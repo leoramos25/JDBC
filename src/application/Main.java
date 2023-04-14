e2eb1a8e939bf66064a50541c5aa9b14da198daa
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-04-14 13:52:28 -0300

    insert java sql command

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,30 +2,46 @@ package application;
 
 import database.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Connection conn;
+        PreparedStatement st = null;
 
         try {
-            connection = DB.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("select * from department");
-            while (resultSet.next()) {
-                System.out.printf("%d | %s%n", resultSet.getInt("Id"), resultSet.getString("Name"));
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "insert into seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, "Leonardo");
+            st.setString(2, "leonardo@email.com");
+            st.setDate(3, new Date(1999, 04, 25));
+            st.setDouble(4, 3800.00);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done, Id = " + id);
+                }
+            } else {
+                System.out.println("No rows affected");
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
         } finally {
-            DB.closeResultSet(resultSet);
-            DB.closeStatement(statement);
             DB.closeConnection();
+            DB.closeStatement(st);
         }
     }
 }
