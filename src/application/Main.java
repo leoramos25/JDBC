37191328b0f2564c4696bdd05e655a8e0a63c68c
Leonardo Ramos de Sousa <leoramosdsousa@gmail.com>
2023-04-16 11:19:34 -0300

    update database data

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,46 +2,31 @@ package application;
 
 import database.DB;
 
-import java.sql.*;
-import java.time.format.DateTimeFormatter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        Connection conn;
-        PreparedStatement st = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
         try {
-            conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "insert into seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-                            + "VALUES "
-                            + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+            connection = DB.getConnection();
+            preparedStatement = connection
+                    .prepareStatement("update seller set BaseSalary = BaseSalary + ? where (DepartmentId = ?)");
 
-            st.setString(1, "Leonardo");
-            st.setString(2, "leonardo@email.com");
-            st.setDate(3, new Date(1999, 04, 25));
-            st.setDouble(4, 3800.00);
-            st.setInt(5, 4);
+            preparedStatement.setDouble(1, 200.00);
+            preparedStatement.setInt(2, 2);
 
-            int rowsAffected = st.executeUpdate();
+            int rowsAffected = preparedStatement.executeUpdate();
 
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done, Id = " + id);
-                }
-            } else {
-                System.out.println("No rows affected");
-            }
+            System.out.println("Done, rows affected " + rowsAffected);
         } catch (SQLException exception) {
             exception.printStackTrace();
         } finally {
+            DB.closeStatement(preparedStatement);
             DB.closeConnection();
-            DB.closeStatement(st);
         }
     }
 }
